f2693107dbb7c629442ac9c4470bc6ae77885cd1
Ioan Latrovaliev <ioan.latrovaliev@lucidlink.com>
2024-05-07 22:25:45 +0300

    insertion sort in nonincreasing order

--- preface/insertion-sort.ts
+++ preface/insertion-sort.ts
@@ -1,6 +1,7 @@
+// 1.1 Insertion sort
+
 function insertionSort(arr: number[]) {
-  for (let j = 0; j <= arr.length - 1; j++) {
-    console.log(arr);
+  for (let j = 1; j <= arr.length - 1; j++) {
     const curr = arr[j];
     let i = j - 1;
 
@@ -16,3 +17,22 @@ function insertionSort(arr: number[]) {
 }
 
 console.log(insertionSort([5, 2, 4, 6, 1, 3]));
+
+// 1.2 Rewrite insertion sort procedure to sort into nonincreasing order.
+function nonIncreasingInsertionSort(arr: number[]) {
+  for (let j = 1; j <= arr.length - 1; j++) {
+    let curr = arr[j];
+    let i = j - 1;
+
+    while (i >= 0 && arr[i] < curr) {
+      arr[i + 1] = arr[i];
+      i--;
+    }
+
+    arr[i + 1] = curr;
+  }
+
+  return arr;
+}
+
+console.log(nonIncreasingInsertionSort([5, 2, 4, 6, 1, 3]));
