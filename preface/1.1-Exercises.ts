2bef0856fe6946df0ec8f42b345f307e5c136125
Ioan Latrovaliev <ioan.latrovaliev@lucidlink.com>
2024-05-20 22:23:21 +0300

    1.1-3

--- preface/1.1-Exercises.ts
+++ preface/1.1-Exercises.ts
@@ -1,5 +1,4 @@
 // 1.1-1 Insertion sort
-
 function insertionSort(arr: number[]) {
   for (let j = 1; j <= arr.length - 1; j++) {
     const curr = arr[j];
@@ -36,3 +35,19 @@ function nonIncreasingInsertionSort(arr: number[]) {
 }
 
 console.log(nonIncreasingInsertionSort([5, 2, 4, 6, 1, 3]));
+
+/* 1.1-3 Consider the searching problem:
+ * Input: A sequence of n numbers A = (a1,a2,...,an) and a value v.
+ * Output: An index i such that v = A[i] or NIL if v does not appear in A.
+ *
+ * Write code for linear search, which scans through the sequence, looking for v.
+ */
+function linearSearch(arr: number[], v: number) {
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === v) return i;
+  }
+
+  return null;
+}
+
+console.log(linearSearch([5, 2, 4, 6, 1, 3], 100));
